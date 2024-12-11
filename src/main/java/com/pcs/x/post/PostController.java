fa1536f70667612e1858ac7deefa42e87f187df8
chandracs238 <chandhu7013@gmail.com>
2024-12-11 16:45:56 +0530

    Third Commit

--- src/main/java/com/pcs/x/post/PostController.java
+++ src/main/java/com/pcs/x/post/PostController.java
@@ -3,20 +3,34 @@ package com.pcs.x.post;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pcs.x.exception.UserNotFoundException;
+import com.pcs.x.user.User;
+import com.pcs.x.user.UserRepository;
 
 @RestController
 public class PostController {
 
 	private PostRepository postRepository;
+	private UserRepository userRepository;
 
-	public PostController(PostRepository postRepository) {
+	public PostController(PostRepository postRepository,UserRepository userRepository) {
 		super();
+		this.userRepository = userRepository;
 		this.postRepository = postRepository;
 	}
 	
-	@GetMapping("/users/posts")
-	public List<Post> allPost(){
-		return postRepository.findAll();
+	@GetMapping("/{username}/posts")
+	public List<Post> allPost(@PathVariable String username){
+		User user = userRepository.findByName(username);
+		
+		if(user == null) {
+			throw new UserNotFoundException(-1l);
+		}
+		
+		List<Post> userPosts = postRepository.findByUser(user);
+		return userPosts;
 	}
 }
--- src/main/java/com/pcs/x/post/PostRepository.java
+++ src/main/java/com/pcs/x/post/PostRepository.java
@@ -1,9 +1,15 @@
 package com.pcs.x.post;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pcs.x.user.User;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
+
+	List<Post> findByUser(User user);
 
 }
